fdc6a3e4b8c5923af377a7dcd5a43f5b7fbb78f2
kwak317 <hojun6901@gmail.com>
2025-09-16 21:36:58 +0900

    Feat: 구글맵 API 완성

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Layout from "./components/layout/layout";
 import Home from "./pages/Home/Home";
 import Signin from "./pages/Signin/Signin";
 import AuthRouter from "./router/AuthRouter/AuthRouter";
+import MainRouter from "./router/MainRouter/MainRouter";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <AuthRouter />
+        <MainRouter />
       </BrowserRouter>
 
       {/* 배경 레이아웃 */}
--- src/router/MainRouter/MainRouter.jsx
+++ src/router/MainRouter/MainRouter.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Maps from "../../pages/googlemap/Maps";
+import Home from "../../pages/home/Home";
+
 
 function MainRouter() {
   return (
     <Routes>
       {/* Routes: 여러 개의 Route를 감싸주는 컨테이너 역할 */}
       <Route path="/" element={<Home />} />
+      <Route path="/maps" element={<Maps />} />
     </Routes>
   );
 }
