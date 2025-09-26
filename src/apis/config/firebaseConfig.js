1a69b5b43b2dce8896745ddcb87b0bb5e691e586
JINHYUN-BRIGHTEVERYDAY <jinibrighteveryday@gmail.com>
2025-09-26 21:48:55 +0900

    feat: 이미지 업로드 시도

--- src/apis/config/firebaseConfig.js
+++ src/apis/config/firebaseConfig.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY ,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD4lvQK4VC4Umc5TFwKQHn3AumdClzPe9M",
+  authDomain: "headers-b2f66.firebaseapp.com",
+  projectId: "headers-b2f66",
+  storageBucket: "headers-b2f66.appspot.com",
+  messagingSenderId: "577659035157",
+  appId: "1:577659035157:web:3c5ce24d01b8b197ebf81c",
+  measurementId: "G-NXX6DL1ZCJ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
 export const storage = getStorage(app);
